d7fad073bb227ab4af2c79916b9a370f6aabfeca
Petra <henry.andersson@tuni.fi>
2025-12-16 19:22:10 +0200

    Github action yml

--- cypress/e2e/albumDetail.cy.ts
+++ cypress/e2e/albumDetail.cy.ts
@@ -1,23 +1,22 @@
-import { setupDefaultRouteMocks } from "../support/utils"
+import { setupDefaultRouteMocks } from '../support/utils';
 
 describe('Album Detail Tests', () => {
-    beforeEach(() => {
-        setupDefaultRouteMocks()
-        cy.visit('/albums/1')
-    })
+  beforeEach(() => {
+    setupDefaultRouteMocks();
+    cy.visit('/albums/1');
+  });
 
-    it('should get album photos', () => {
-        cy.get('#photos-grid').should('be.visible')
-        cy.get('#photos-grid').children().first().should('exist')
-    })
+  it('should get album photos', () => {
+    cy.get('#photos-grid').should('be.visible');
+    cy.get('#photos-grid').children().first().should('exist');
+  });
 
-    it('should display album title', () => {
-        cy.contains('h1', 'Album 1').should('be.visible')
-    })
+  it('should display album title', () => {
+    cy.contains('h1', 'Album 1').should('be.visible');
+  });
 
-    it('should display photo details when a photo is clicked', () => {
-        cy.get('#photos-grid button').first().click()
-        cy.url().should('include', '/photo/1')
-
-    })
-})
+  it('should display photo details when a photo is clicked', () => {
+    cy.get('#photos-grid button').first().click();
+    cy.url().should('include', '/photo/1');
+  });
+});
