ec2c0b56c273606ecc00fdcbaa909e5239fc491b
Petra <henry.andersson@tuni.fi>
2025-12-16 19:45:44 +0200

    Fixed wrong base url in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://henewastaken.github.io/photo-browser/',
   build: {
     outDir: 'dist',
   },
