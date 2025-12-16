623aeb7282e75cc2f6506a95437198f141d97016
Petra <henry.andersson@tuni.fi>
2025-12-16 19:40:28 +0200

    Fixed wrong base url in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/photo_browser/',
+  base: '/',
   plugins: [react()],
   server: {
     hmr: {
